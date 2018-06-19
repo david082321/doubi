80455956e78767bb1f9c0f5506a4069b762a4ee1
david082321 <7046617+david082321@users.noreply.github.com>
2018-06-20 03:33:42 +0800

    Update ban_iptables.sh

--- ban_iptables.sh
+++ ban_iptables.sh
@@ -3,15 +3,15 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 #=================================================
 #       System Required: CentOS/Debian/Ubuntu
-#       Description: iptables 封禁 BT、PT、SPAM（垃圾邮件）和自定义端口、关键词
+#       Description: iptables 封禁 BT、PT、SPAM（垃圾郵件）和自訂埠、關鍵字
 #       Version: 1.0.10
 #       Blog: https://doub.io/shell-jc2/
 #=================================================
 
 sh_ver="1.0.10"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
-Info="${Green_font_prefix}[信息]${Font_color_suffix}"
-Error="${Red_font_prefix}[错误]${Font_color_suffix}"
+Info="${Green_font_prefix}[訊息]${Font_color_suffix}"
+Error="${Red_font_prefix}[錯誤]${Font_color_suffix}"
 
 smtp_port="25,26,465,587"
 pop3_port="109,110,995"
@@ -75,12 +75,12 @@ Cat_KEY_WORDS(){
 }
 View_PORT(){
 	Cat_PORT
-	echo -e "===============${Red_background_prefix} 当前已封禁 端口 ${Font_color_suffix}==============="
+	echo -e "===============${Red_background_prefix} 目前已封禁 埠 ${Font_color_suffix}==============="
 	echo -e "$Ban_PORT_list" && echo && echo -e "==============================================="
 }
 View_KEY_WORDS(){
 	Cat_KEY_WORDS
-	echo -e "==============${Red_background_prefix} 当前已封禁 关键词 ${Font_color_suffix}=============="
+	echo -e "==============${Red_background_prefix} 目前已封禁 關鍵字 ${Font_color_suffix}=============="
 	echo -e "$Ban_KEY_WORDS_list" && echo -e "==============================================="
 }
 View_ALL(){
@@ -172,19 +172,19 @@ Set_ALL(){
 }
 Ban_BT(){
 	check_BT
-	[[ ! -z ${BT_KEY_WORDS} ]] && echo -e "${Error} 检测到已封禁BT、PT 关键词，无需再次封禁 !" && exit 0
+	[[ ! -z ${BT_KEY_WORDS} ]] && echo -e "${Error} 檢測到已封禁BT、PT 關鍵字，無需再次封禁 !" && exit 0
 	s="A"
 	Set_BT
 	View_ALL
-	echo -e "${Info} 已封禁BT、PT 关键词 !"
+	echo -e "${Info} 已封禁BT、PT 關鍵字 !"
 }
 Ban_SPAM(){
 	check_SPAM
-	[[ ! -z ${SPAM_PORT} ]] && echo -e "${Error} 检测到已封禁SPAM(垃圾邮件) 端口，无需再次封禁 !" && exit 0
+	[[ ! -z ${SPAM_PORT} ]] && echo -e "${Error} 檢測到已封禁SPAM(垃圾郵件) 埠，無需再次封禁 !" && exit 0
 	s="A"
 	Set_SPAM
 	View_ALL
-	echo -e "${Info} 已封禁SPAM(垃圾邮件) 端口 !"
+	echo -e "${Info} 已封禁SPAM(垃圾郵件) 埠 !"
 }
 Ban_ALL(){
 	check_BT
@@ -194,37 +194,37 @@ Ban_ALL(){
 		if [[ -z ${SPAM_PORT} ]]; then
 			Set_ALL
 			View_ALL
-			echo -e "${Info} 已封禁BT、PT 关键词 和 SPAM(垃圾邮件) 端口 !"
+			echo -e "${Info} 已封禁BT、PT 關鍵字 和 SPAM(垃圾郵件) 埠 !"
 		else
 			Set_BT
 			View_ALL
-			echo -e "${Info} 已封禁BT、PT 关键词 !"
+			echo -e "${Info} 已封禁BT、PT 關鍵字 !"
 		fi
 	else
 		if [[ -z ${SPAM_PORT} ]]; then
 			Set_SPAM
 			View_ALL
-			echo -e "${Info} 已封禁SPAM(垃圾邮件) 端口 !"
+			echo -e "${Info} 已封禁SPAM(垃圾郵件) 埠 !"
 		else
-			echo -e "${Error} 检测到已封禁BT、PT 关键词 和 SPAM(垃圾邮件) 端口，无需再次封禁 !" && exit 0
+			echo -e "${Error} 檢測到已封禁BT、PT 關鍵字 和 SPAM(垃圾郵件) 埠，無需再次封禁 !" && exit 0
 		fi
 	fi
 }
 UnBan_BT(){
 	check_BT
-	[[ -z ${BT_KEY_WORDS} ]] && echo -e "${Error} 检测到未封禁BT、PT 关键词，请检查 !" && exit 0
+	[[ -z ${BT_KEY_WORDS} ]] && echo -e "${Error} 檢測到未封禁BT、PT 關鍵字，請檢查 !" && exit 0
 	s="D"
 	Set_BT
 	View_ALL
-	echo -e "${Info} 已解封BT、PT 关键词 !"
+	echo -e "${Info} 已解封BT、PT 關鍵字 !"
 }
 UnBan_SPAM(){
 	check_SPAM
-	[[ -z ${SPAM_PORT} ]] && echo -e "${Error} 检测到未封禁SPAM(垃圾邮件) 端口，请检查 !" && exit 0
+	[[ -z ${SPAM_PORT} ]] && echo -e "${Error} 檢測到未封禁SPAM(垃圾郵件) 埠，請檢查 !" && exit 0
 	s="D"
 	Set_SPAM
 	View_ALL
-	echo -e "${Info} 已解封SPAM(垃圾邮件) 端口 !"
+	echo -e "${Info} 已解封SPAM(垃圾郵件) 埠 !"
 }
 UnBan_ALL(){
 	check_BT
@@ -234,19 +234,19 @@ UnBan_ALL(){
 		if [[ ! -z ${SPAM_PORT} ]]; then
 			Set_ALL
 			View_ALL
-			echo -e "${Info} 已解封BT、PT 关键词 和 SPAM(垃圾邮件) 端口 !"
+			echo -e "${Info} 已解封BT、PT 關鍵字 和 SPAM(垃圾郵件) 埠 !"
 		else
 			Set_BT
 			View_ALL
-			echo -e "${Info} 已解封BT、PT 关键词 !"
+			echo -e "${Info} 已解封BT、PT 關鍵字 !"
 		fi
 	else
 		if [[ ! -z ${SPAM_PORT} ]]; then
 			Set_SPAM
 			View_ALL
-			echo -e "${Info} 已解封SPAM(垃圾邮件) 端口 !"
+			echo -e "${Info} 已解封SPAM(垃圾郵件) 埠 !"
 		else
-			echo -e "${Error} 检测到未封禁BT、PT 关键词和 SPAM(垃圾邮件) 端口，请检查 !" && exit 0
+			echo -e "${Error} 檢測到未封禁BT、PT 關鍵字和 SPAM(垃圾郵件) 埠，請檢查 !" && exit 0
 		fi
 	fi
 }
@@ -254,11 +254,11 @@ ENTER_Ban_KEY_WORDS_type(){
 	Type=$1
 	Type_1=$2
 	if [[ $Type_1 != "ban_1" ]]; then
-		echo -e "请选择输入类型：
- 1. 手动输入（只支持单个关键词）
- 2. 本地文件读取（支持批量读取关键词，每行一个关键词）
- 3. 网络地址读取（支持批量读取关键词，每行一个关键词）" && echo
-		stty erase '^H' && read -p "(默认: 1. 手动输入):" key_word_type
+		echo -e "請選擇輸入類型：
+ 1. 手動輸入（只支援單個關鍵字）
+ 2. 本機檔案讀取（支援批次讀取關鍵字，每行一個關鍵字）
+ 3. 網路地址讀取（支援批次讀取關鍵字，每行一個關鍵字）" && echo
+		stty erase '^H' && read -p "(預設: 1. 手動輸入):" key_word_type
 	fi
 	[[ -z "${key_word_type}" ]] && key_word_type="1"
 	if [[ ${key_word_type} == "1" ]]; then
@@ -280,68 +280,68 @@ ENTER_Ban_KEY_WORDS_type(){
 	fi
 }
 ENTER_Ban_PORT(){
-	echo -e "请输入欲封禁的 端口（单端口/多端口/连续端口段）"
+	echo -e "請輸入欲封禁的 埠（單埠/多埠/連續埠段）"
 	if [[ ${Ban_PORT_Type_1} != "1" ]]; then
-	echo -e "${Green_font_prefix}========示例说明========${Font_color_suffix}
- 单端口：25（单个端口）
- 多端口：25,26,465,587（多个端口用英文逗号分割）
- 连续端口段：25:587（25-587之间的所有端口）" && echo
+	echo -e "${Green_font_prefix}========範例說明========${Font_color_suffix}
+ 單埠：25（單個埠）
+ 多埠：25,26,465,587（多個埠用英文逗號分割）
+ 連續埠段：25:587（25-587之間的所有埠）" && echo
 	fi
-	stty erase '^H' && read -p "(回车默认取消):" PORT
+	stty erase '^H' && read -p "(回車預設取消):" PORT
 	[[ -z "${PORT}" ]] && echo "已取消..." && View_ALL && exit 0
 }
 ENTER_Ban_KEY_WORDS(){
-	echo -e "请输入欲封禁的 关键词（域名等，仅支持单个关键词）"
+	echo -e "請輸入欲封禁的 關鍵字（域名等，僅支援單個關鍵字）"
 	if [[ ${Type_1} != "ban_1" ]]; then
-	echo -e "${Green_font_prefix}========示例说明========${Font_color_suffix}
- 关键词：youtube，即禁止访问任何包含关键词 youtube 的域名。
- 关键词：youtube.com，即禁止访问任何包含关键词 youtube.com 的域名（泛域名屏蔽）。
- 关键词：www.youtube.com，即禁止访问任何包含关键词 www.youtube.com 的域名（子域名屏蔽）。
- 更多效果自行测试（如关键词 .zip 即可禁止下载任何 .zip 后缀的文件）。" && echo
+	echo -e "${Green_font_prefix}========範例說明========${Font_color_suffix}
+ 關鍵字：youtube，即禁止訪問任何包含關鍵字 youtube 的域名。
+ 關鍵字：youtube.com，即禁止訪問任何包含關鍵字 youtube.com 的域名（泛域名封鎖）。
+ 關鍵字：www.youtube.com，即禁止訪問任何包含關鍵字 www.youtube.com 的域名（子域名封鎖）。
+ 更多效果自行測試（如關鍵字 .zip 即可禁止下載任何 .zip 後綴的檔案）。" && echo
 	fi
-	stty erase '^H' && read -p "(回车默认取消):" key_word
+	stty erase '^H' && read -p "(回車預設取消):" key_word
 	[[ -z "${key_word}" ]] && echo "已取消..." && View_ALL && exit 0
 }
 ENTER_Ban_KEY_WORDS_file(){
-	echo -e "请输入欲封禁/解封的 关键词本地文件（请使用绝对路径）" && echo
-	stty erase '^H' && read -p "(默认 读取脚本同目录下的 key_word.txt ):" key_word
+	echo -e "請輸入欲封禁/解封的 關鍵字本機檔案（請使用絕對路徑）" && echo
+	stty erase '^H' && read -p "(預設 讀取腳本同目錄下的 key_word.txt ):" key_word
 	[[ -z "${key_word}" ]] && key_word="key_word.txt"
 	if [[ -e "${key_word}" ]]; then
 		key_word=$(cat "${key_word}")
-		[[ -z ${key_word} ]] && echo -e "${Error} 文件内容为空 !" && View_ALL && exit 0
+		[[ -z ${key_word} ]] && echo -e "${Error} 檔案內容為空 !" && View_ALL && exit 0
 	else
-		echo -e "${Error} 没有找到文件 ${key_word} !" && View_ALL && exit 0
+		echo -e "${Error} 沒有找到檔案 ${key_word} !" && View_ALL && exit 0
 	fi
 }
 ENTER_Ban_KEY_WORDS_url(){
-	echo -e "请输入欲封禁/解封的 关键词网络文件地址（例如 http://xxx.xx/key_word.txt）" && echo
-	stty erase '^H' && read -p "(回车默认取消):" key_word
+	echo -e "請輸入欲封禁/解封的 關鍵字網路檔案地址（例如 http://xxx.xx/key_word.txt）" && echo
+	stty erase '^H' && read -p "(回車預設取消):" key_word
 	[[ -z "${key_word}" ]] && echo "已取消..." && View_ALL && exit 0
 	key_word=$(wget --no-check-certificate -t3 -T5 -qO- "${key_word}")
-	[[ -z ${key_word} ]] && echo -e "${Error} 网络文件内容为空或访问超时 !" && View_ALL && exit 0
+	[[ -z ${key_word} ]] && echo -e "${Error} 網路檔案內容為空或訪問超時 !" && View_ALL && exit 0
 }
 ENTER_UnBan_KEY_WORDS(){
 	View_KEY_WORDS
-	echo -e "请输入欲解封的 关键词（根据上面的列表输入完整准确的 关键词）" && echo
-	stty erase '^H' && read -p "(回车默认取消):" key_word
+	echo -e "請輸入欲解封的 關鍵字（根據上面的列表輸入完整準確的 關鍵字）" && echo
+	stty erase '^H' && read -p "(回車預設取消):" key_word
 	[[ -z "${key_word}" ]] && echo "已取消..." && View_ALL && exit 0
 }
 ENTER_UnBan_PORT(){
-	echo -e "请输入欲解封的 端口（根据上面的列表输入完整准确的 端口，包括逗号、冒号）" && echo
-	stty erase '^H' && read -p "(回车默认取消):" PORT
+	echo -e "請輸入欲解封的 埠（根據上面的列表輸入完整準確的 埠，包括逗號、冒號）" && echo
+	stty erase '^H' && read -p "(回車預設取消):" PORT
 	[[ -z "${PORT}" ]] && echo "已取消..." && View_ALL && exit 0
 }
 Ban_PORT(){
 	s="A"
 	ENTER_Ban_PORT
 	Set_PORT
-	echo -e "${Info} 已封禁端口 [ ${PORT} ] !\n"
+	echo -e "${Info} 已封禁埠 [ ${PORT} ] !\n"
 	Ban_PORT_Type_1="1"
 	while true
 	do
 		ENTER_Ban_PORT
 		Set_PORT
-		echo -e "${Info} 已封禁端口 [ ${PORT} ] !\n"
+		echo -e "${Info} 已封禁埠 [ ${PORT} ] !\n"
 	done
 	View_ALL
 }
@@ -349,52 +349,52 @@ Ban_KEY_WORDS(){
 	s="A"
 	ENTER_Ban_KEY_WORDS_type "ban"
 	Set_KEY_WORDS
-	echo -e "${Info} 已封禁关键词 [ ${key_word} ] !\n"
+	echo -e "${Info} 已封禁關鍵字 [ ${key_word} ] !\n"
 	while true
 	do
 		ENTER_Ban_KEY_WORDS_type "ban" "ban_1"
 		Set_KEY_WORDS
-		echo -e "${Info} 已封禁关键词 [ ${key_word} ] !\n"
+		echo -e "${Info} 已封禁關鍵字 [ ${key_word} ] !\n"
 	done
 	View_ALL
 }
 UnBan_PORT(){
 	s="D"
 	View_PORT
-	[[ -z ${Ban_PORT_list} ]] && echo -e "${Error} 检测到未封禁任何 端口 !" && exit 0
+	[[ -z ${Ban_PORT_list} ]] && echo -e "${Error} 檢測到未封禁任何 埠 !" && exit 0
 	ENTER_UnBan_PORT
 	Set_PORT
-	echo -e "${Info} 已解封端口 [ ${PORT} ] !\n"
+	echo -e "${Info} 已解封埠 [ ${PORT} ] !\n"
 	while true
 	do
 		View_PORT
-		[[ -z ${Ban_PORT_list} ]] && echo -e "${Error} 检测到未封禁任何 端口 !" && exit 0
+		[[ -z ${Ban_PORT_list} ]] && echo -e "${Error} 檢測到未封禁任何 埠 !" && exit 0
 		ENTER_UnBan_PORT
 		Set_PORT
-		echo -e "${Info} 已解封端口 [ ${PORT} ] !\n"
+		echo -e "${Info} 已解封埠 [ ${PORT} ] !\n"
 	done
 	View_ALL
 }
 UnBan_KEY_WORDS(){
 	s="D"
 	Cat_KEY_WORDS
-	[[ -z ${Ban_KEY_WORDS_list} ]] && echo -e "${Error} 检测到未封禁任何 关键词 !" && exit 0
+	[[ -z ${Ban_KEY_WORDS_list} ]] && echo -e "${Error} 檢測到未封禁任何 關鍵字 !" && exit 0
 	ENTER_Ban_KEY_WORDS_type "unban"
 	Set_KEY_WORDS
-	echo -e "${Info} 已解封关键词 [ ${key_word} ] !\n"
+	echo -e "${Info} 已解封關鍵字 [ ${key_word} ] !\n"
 	while true
 	do
 		Cat_KEY_WORDS
-		[[ -z ${Ban_KEY_WORDS_list} ]] && echo -e "${Error} 检测到未封禁任何 关键词 !" && exit 0
+		[[ -z ${Ban_KEY_WORDS_list} ]] && echo -e "${Error} 檢測到未封禁任何 關鍵字 !" && exit 0
 		ENTER_Ban_KEY_WORDS_type "unban" "ban_1"
 		Set_KEY_WORDS
-		echo -e "${Info} 已解封关键词 [ ${key_word} ] !\n"
+		echo -e "${Info} 已解封關鍵字 [ ${key_word} ] !\n"
 	done
 	View_ALL
 }
 UnBan_KEY_WORDS_ALL(){
 	Cat_KEY_WORDS
-	[[ -z ${Ban_KEY_WORDS_text} ]] && echo -e "${Error} 检测到未封禁任何 关键词，请检查 !" && exit 0
+	[[ -z ${Ban_KEY_WORDS_text} ]] && echo -e "${Error} 檢測到未封禁任何 關鍵字，請檢查 !" && exit 0
 	if [[ ! -z "${v6iptables}" ]]; then
 		Ban_KEY_WORDS_v6_num=$(echo -e "${Ban_KEY_WORDS_v6_list}"|wc -l)
 		for((integer = 1; integer <= ${Ban_KEY_WORDS_v6_num}; integer++))
@@ -409,7 +409,7 @@ UnBan_KEY_WORDS_ALL(){
 	done
 	Save_iptables_v4_v6
 	View_ALL
-	echo -e "${Info} 已解封所有关键词 !"
+	echo -e "${Info} 已解封所有關鍵字 !"
 }
 check_iptables(){
 	v4iptables=`iptables -V`
@@ -420,28 +420,28 @@ check_iptables(){
 			v6iptables="ip6tables"
 		fi
 	else
-		echo -e "${Error} 未安装 iptables 防火墙 !
-请安装 iptables防火墙：
-CentOS 系统：yum install iptables -y
-Debian / Ubuntu 系统：apt-get install iptables -y"
+		echo -e "${Error} 未安裝 iptables 防火牆 !
+請安裝 iptables防火牆：
+CentOS 系統：yum install iptables -y
+Debian / Ubuntu 系統：apt-get install iptables -y"
 	fi
 }
 Update_Shell(){
-	echo -e "当前版本为 [ ${sh_ver} ]，开始检测最新版本..."
+	echo -e "目前版本為 [ ${sh_ver} ]，開始檢測最新版本..."
 	sh_new_ver=$(wget --no-check-certificate -qO- "https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/ban_iptables.sh"|grep 'sh_ver="'|awk -F "=" '{print $NF}'|sed 's/\"//g'|head -1)
-	[[ -z ${sh_new_ver} ]] && echo -e "${Error} 检测最新版本失败 !" && exit 1
+	[[ -z ${sh_new_ver} ]] && echo -e "${Error} 檢測最新版本失敗 !" && exit 1
 	if [[ ${sh_new_ver} != ${sh_ver} ]]; then
-		echo -e "发现新版本[ ${sh_new_ver} ]，是否更新？[Y/n]"
-		stty erase '^H' && read -p "(默认: y):" yn
+		echo -e "發現新版本[ ${sh_new_ver} ]，是否更新？[Y/n]"
+		stty erase '^H' && read -p "(預設: y):" yn
 		[[ -z "${yn}" ]] && yn="y"
 		if [[ ${yn} == [Yy] ]]; then
 			wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/ban_iptables.sh && chmod +x ban_iptables.sh
-			echo -e "脚本已更新为最新版本[ ${sh_new_ver} ] !"
+			echo -e "腳本已更新為最新版本[ ${sh_new_ver} ] !"
 		else
 			echo && echo "	已取消..." && echo
 		fi
 	else
-		echo -e "当前已是最新版本[ ${sh_new_ver} ] !"
+		echo -e "目前已是最新版本[ ${sh_new_ver} ] !"
 	fi
 }
 check_sys
@@ -455,27 +455,27 @@ if [[ ! -z $action ]]; then
 	[[ $action = "unbanspam" ]] && UnBan_SPAM && exit 0
 	[[ $action = "unbanall" ]] && UnBan_ALL && exit 0
 fi
-echo && echo -e " iptables防火墙 封禁管理脚本 ${Red_font_prefix}[v${sh_ver}]${Font_color_suffix}
+echo && echo -e " iptables防火牆 封禁管理腳本 ${Red_font_prefix}[v${sh_ver}]${Font_color_suffix}
   -- Toyo | doub.io/shell-jc2 --
 
-  ${Green_font_prefix}0.${Font_color_suffix} 查看 当前封禁列表
+  ${Green_font_prefix}0.${Font_color_suffix} 查看 目前封禁列表
 ————————————
   ${Green_font_prefix}1.${Font_color_suffix} 封禁 BT、PT
-  ${Green_font_prefix}2.${Font_color_suffix} 封禁 SPAM(垃圾邮件)
+  ${Green_font_prefix}2.${Font_color_suffix} 封禁 SPAM(垃圾郵件)
   ${Green_font_prefix}3.${Font_color_suffix} 封禁 BT、PT+SPAM
-  ${Green_font_prefix}4.${Font_color_suffix} 封禁 自定义  端口
-  ${Green_font_prefix}5.${Font_color_suffix} 封禁 自定义关键词
+  ${Green_font_prefix}4.${Font_color_suffix} 封禁 自訂  埠
+  ${Green_font_prefix}5.${Font_color_suffix} 封禁 自訂關鍵字
 ————————————
   ${Green_font_prefix}6.${Font_color_suffix} 解封 BT、PT
-  ${Green_font_prefix}7.${Font_color_suffix} 解封 SPAM(垃圾邮件)
+  ${Green_font_prefix}7.${Font_color_suffix} 解封 SPAM(垃圾郵件)
   ${Green_font_prefix}8.${Font_color_suffix} 解封 BT、PT+SPAM
-  ${Green_font_prefix}9.${Font_color_suffix} 解封 自定义  端口
- ${Green_font_prefix}10.${Font_color_suffix} 解封 自定义关键词
- ${Green_font_prefix}11.${Font_color_suffix} 解封 所有  关键词
+  ${Green_font_prefix}9.${Font_color_suffix} 解封 自訂  埠
+ ${Green_font_prefix}10.${Font_color_suffix} 解封 自訂關鍵字
+ ${Green_font_prefix}11.${Font_color_suffix} 解封 所有  關鍵字
 ————————————
- ${Green_font_prefix}12.${Font_color_suffix} 升级脚本
+ ${Green_font_prefix}12.${Font_color_suffix} 升級腳本
 " && echo
-stty erase '^H' && read -p " 请输入数字 [0-12]:" num
+stty erase '^H' && read -p " 請輸入數字 [0-12]:" num
 case "$num" in
 	0)
 	View_ALL
@@ -517,6 +517,6 @@ case "$num" in
 	Update_Shell
 	;;
 	*)
-	echo "请输入正确数字 [0-12]"
+	echo "請輸入正確數字 [0-12]"
 	;;
 esac
