d626657a04c31b4da6a5257a61a1db8eb8ce22b1
david082321 <7046617+david082321@users.noreply.github.com>
2018-06-20 03:34:18 +0800

    Update adbyby.sh

--- adbyby.sh
+++ adbyby.sh
@@ -15,11 +15,11 @@ adbyby_file="/usr/local/adbyby/bin/adbyby"
 adbyby_conf="/usr/local/adbyby/bin/adhook.ini"
 
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
-Info="${Green_font_prefix}[信息]${Font_color_suffix}"
-Error="${Red_font_prefix}[错误]${Font_color_suffix}"
+Info="${Green_font_prefix}[訊息]${Font_color_suffix}"
+Error="${Red_font_prefix}[錯誤]${Font_color_suffix}"
 Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
 
-#检查系统
+#檢查系統
 check_sys(){
 	if [[ -f /etc/redhat-release ]]; then
 		release="centos"
@@ -39,7 +39,7 @@ check_sys(){
 	bit=`uname -m`
 }
 check_installed_status(){
-	[[ ! -e ${adbyby_file} ]] && echo -e "${Error} ADbyby 没有安装，请检查 !" && exit 1
+	[[ ! -e ${adbyby_file} ]] && echo -e "${Error} ADbyby 沒有安裝，請檢查 !" && exit 1
 }
 check_pid(){
 	PID=`ps -ef| grep "adbyby"| grep -v grep| grep -v ".sh"| grep -v "init.d"| grep -v "service"| awk '{print $2}'`
@@ -51,9 +51,9 @@ Download_adbyby(){
 	else
 		wget --no-check-certificate -O "adbyby.tar.gz" "https://raw.githubusercontent.com/adbyby/Files/master/linux.86.tar.gz"
 	fi
-	[[ ! -e "adbyby.tar.gz" ]] && echo -e "${Error} ADbyby 下载失败 !" && exit 1
+	[[ ! -e "adbyby.tar.gz" ]] && echo -e "${Error} ADbyby 下載失敗 !" && exit 1
 	tar -xzf adbyby.tar.gz && rm -rf adbyby.tar.gz
-	[[ ! -e "${adbyby_file}" ]] && echo -e "${Error} ADbyby 解压失败 !" && exit 1
+	[[ ! -e "${adbyby_file}" ]] && echo -e "${Error} ADbyby 解壓失敗 !" && exit 1
 	cd bin
 	chmod 777 adbyby
 	wget -N --no-check-certificate "https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/other/adhook.ini"
@@ -61,19 +61,19 @@ Download_adbyby(){
 Service_adbyby(){
 	if [[ ${release} = "centos" ]]; then
 		if ! wget --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/other/adbyby_centos -O /etc/init.d/adbyby; then
-			echo -e "${Error} ADbyby服务 管理脚本下载失败 !" && exit 1
+			echo -e "${Error} ADbyby服務 管理腳本下載失敗 !" && exit 1
 		fi
 		chmod +x /etc/init.d/adbyby
 		chkconfig --add adbyby
 		chkconfig adbyby on
 	else
 		if ! wget --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/other/adbyby_debian -O /etc/init.d/adbyby; then
-			echo -e "${Error} ADbyby服务 管理脚本下载失败 !" && exit 1
+			echo -e "${Error} ADbyby服務 管理腳本下載失敗 !" && exit 1
 		fi
 		chmod +x /etc/init.d/adbyby
 		update-rc.d -f adbyby defaults
 	fi
-	echo -e "${Info} ADbyby服务 管理脚本下载完成 !"
+	echo -e "${Info} ADbyby服務 管理腳本下載完成 !"
 }
 Installation_dependency(){
 	if [[ ${release} = "centos" ]]; then
@@ -86,33 +86,33 @@ Installation_dependency(){
 	mkdir ${file}
 }
 Install_adbyby(){
-	[[ -e ${adbyby_file} ]] && echo -e "${Error} 检测到 ADbyby 已安装 !" && exit 1
+	[[ -e ${adbyby_file} ]] && echo -e "${Error} 檢測到 ADbyby 已安裝 !" && exit 1
 	check_sys
-	echo -e "${Info} 开始安装/配置 依赖..."
+	echo -e "${Info} 開始安裝/設定 依賴..."
 	Installation_dependency
-	echo -e "${Info} 开始下载/安装..."
+	echo -e "${Info} 開始下載/安裝..."
 	Download_adbyby
-	echo -e "${Info} 开始下载/安装 服务脚本(init)..."
+	echo -e "${Info} 開始下載/安裝 服務腳本(init)..."
 	Service_adbyby
-	echo -e "${Info} 开始设置 iptables防火墙..."
+	echo -e "${Info} 開始設定 iptables防火牆..."
 	Set_iptables
-	echo -e "${Info} 开始添加 iptables防火墙规则..."
+	echo -e "${Info} 開始添加 iptables防火牆規則..."
 	Add_iptables
-	echo -e "${Info} 开始保存 iptables防火墙规则..."
+	echo -e "${Info} 開始儲存 iptables防火牆規則..."
 	Save_iptables
-	echo -e "${Info} 所有步骤 安装完毕，开始启动..."
+	echo -e "${Info} 所有步驟 安裝完畢，開始啟動..."
 	Start_adbyby
 }
 Start_adbyby(){
 	check_installed_status
 	check_pid
-	[[ ! -z ${PID} ]] && echo -e "${Error} ADbyby 正在运行，请检查 !" && exit 1
+	[[ ! -z ${PID} ]] && echo -e "${Error} ADbyby 正在執行，請檢查 !" && exit 1
 	service adbyby start
 }
 Stop_adbyby(){
 	check_installed_status
 	check_pid
-	[[ -z ${PID} ]] && echo -e "${Error} ADbyby 没有运行，请检查 !" && exit 1
+	[[ -z ${PID} ]] && echo -e "${Error} ADbyby 沒有執行，請檢查 !" && exit 1
 	service adbyby stop
 }
 Restart_adbyby(){
@@ -128,9 +128,9 @@ Set_adbyby(){
 }
 Uninstall_adbyby(){
 	check_installed_status
-	echo "确定要卸载 ADbyby ? (y/N)"
+	echo "確定要移除 ADbyby ? (y/N)"
 	echo
-	stty erase '^H' && read -p "(默认: n):" unyn
+	stty erase '^H' && read -p "(預設: n):" unyn
 	[[ -z ${unyn} ]] && unyn="n"
 	if [[ ${unyn} == [Yy] ]]; then
 		check_pid
@@ -143,9 +143,9 @@ Uninstall_adbyby(){
 		else
 			update-rc.d -f adbyby remove
 		fi
-		echo && echo "ADbyby 卸载完成 !" && echo
+		echo && echo "ADbyby 移除完成 !" && echo
 	else
-		echo && echo "卸载已取消..." && echo
+		echo && echo "移除已取消..." && echo
 	fi
 }
 Add_iptables(){
@@ -173,29 +173,29 @@ Set_iptables(){
 		chmod +x /etc/network/if-pre-up.d/iptables
 	fi
 }
-echo && echo -e "请输入一个数字来选择选项
+echo && echo -e "請輸入一個數字來選擇選項
 
- ${Green_font_prefix}1.${Font_color_suffix} 安装 ADbyby
- ${Green_font_prefix}2.${Font_color_suffix} 卸载 ADbyby
+ ${Green_font_prefix}1.${Font_color_suffix} 安裝 ADbyby
+ ${Green_font_prefix}2.${Font_color_suffix} 移除 ADbyby
 ————————————
- ${Green_font_prefix}3.${Font_color_suffix} 启动 ADbyby
+ ${Green_font_prefix}3.${Font_color_suffix} 啟動 ADbyby
  ${Green_font_prefix}4.${Font_color_suffix} 停止 ADbyby
- ${Green_font_prefix}5.${Font_color_suffix} 重启 ADbyby
+ ${Green_font_prefix}5.${Font_color_suffix} 重啟 ADbyby
 ————————————
- ${Green_font_prefix}6.${Font_color_suffix} 修改 配置文件
+ ${Green_font_prefix}6.${Font_color_suffix} 修改 設定檔案
 ————————————" && echo
 if [[ -e ${adbyby_file} ]]; then
 	check_pid
 	if [[ ! -z "${PID}" ]]; then
-		echo -e " 当前状态: ${Green_font_prefix}已安装${Font_color_suffix} 并 ${Green_font_prefix}已启动${Font_color_suffix}"
+		echo -e " 目前狀態: ${Green_font_prefix}已安裝${Font_color_suffix} 並 ${Green_font_prefix}已啟動${Font_color_suffix}"
 	else
-		echo -e " 当前状态: ${Green_font_prefix}已安装${Font_color_suffix} 但 ${Red_font_prefix}未启动${Font_color_suffix}"
+		echo -e " 目前狀態: ${Green_font_prefix}已安裝${Font_color_suffix} 但 ${Red_font_prefix}未啟動${Font_color_suffix}"
 	fi
 else
-	echo -e " 当前状态: ${Red_font_prefix}未安装${Font_color_suffix}"
+	echo -e " 目前狀態: ${Red_font_prefix}未安裝${Font_color_suffix}"
 fi
 echo
-stty erase '^H' && read -p " 请输入数字 [1-8]:" num
+stty erase '^H' && read -p " 請輸入數字 [1-8]:" num
 case "$num" in
 	1)
 	Install_adbyby
@@ -216,6 +216,6 @@ case "$num" in
 	Set_adbyby
 	;;
 	*)
-	echo "请输入正确数字 [1-8]"
+	echo "請輸入正確數字 [1-8]"
 	;;
 esac
