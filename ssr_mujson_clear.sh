a4f54cce32707b4a13cf76e4f5ff5d7c39d83359
david082321 <7046617+david082321@users.noreply.github.com>
2018-06-20 03:26:25 +0800

    Update ssr_mujson_clear.sh

--- ssr_mujson_clear.sh
+++ ssr_mujson_clear.sh
@@ -9,27 +9,27 @@ export PATH
 #	Author: Toyo
 #=================================================
 SSR_file="/usr/local/shadowsocksr"
-# 这里填写 mujson_mgr.py 文件的上层绝对路径
-Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Font_color_suffix="\033[0m" && Info="${Green_font_prefix}[信息]${Font_color_suffix}" && Error="${Red_font_prefix}[错误]${Font_color_suffix}" && Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
+# 這裡填寫 mujson_mgr.py 檔案的上層絕對路徑
+Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Font_color_suffix="\033[0m" && Info="${Green_font_prefix}[訊息]${Font_color_suffix}" && Error="${Red_font_prefix}[錯誤]${Font_color_suffix}" && Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
 check_ssr(){
-	[[ ! -e ${SSR_file} ]] && echo -e "${Error} mujson_mgr.py 文件不存在或变量设定错误 !" && exit 1
+	[[ ! -e ${SSR_file} ]] && echo -e "${Error} mujson_mgr.py 檔案不存在或變數設定錯誤 !" && exit 1
 }
 scan_port(){
 	cd "${SSR_file}"
 	port_all=$(python "mujson_mgr.py" -l)
-	[[ -z ${port_all} ]] && echo -e "${Error} 没有发现任何端口(用户) !" && exit 1
+	[[ -z ${port_all} ]] && echo -e "${Error} 沒有發現任何埠(使用者) !" && exit 1
 	port_num=$(echo "${port_all}"|wc -l)
-	[[ ${port_num} = 0 ]] && echo -e "${Error} 没有发现任何端口(用户) !" && exit 1
+	[[ ${port_num} = 0 ]] && echo -e "${Error} 沒有發現任何埠(使用者) !" && exit 1
 }
 clear_traffic(){
 	for((integer = 1; integer <= ${port_num}; integer++))
 	do
 		port=$(echo -e "${port_all}"|sed -n "${integer}p"|awk '{print $NF}')
-		[[ -z ${port} ]] && echo -e "${Error} 获取的端口(用户)为空 !" && exit 1
+		[[ -z ${port} ]] && echo -e "${Error} 獲取的埠(使用者)為空 !" && exit 1
 		result=$(python "mujson_mgr.py" -c -p "${port}")
-		echo -e "${Info} 端口[${port}] 流量已清零 !"
+		echo -e "${Info} 埠[${port}] 流量已清零 !"
 	done
-	echo -e "${Info} 所有端口(用户)流量已清零 !"
+	echo -e "${Info} 所有埠(使用者)流量已清零 !"
 }
 c_ssr(){
 	check_ssr
@@ -43,7 +43,7 @@ case "$action" in
     ${action}_ssr
     ;;
     *)
-    echo -e "输入错误 !
- 用法: c 清空 所有用户已使用流量"
+    echo -e "輸入錯誤 !
+ 用法: c 清空 所有使用者已使用流量"
     ;;
 esac
