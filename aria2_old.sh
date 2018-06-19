d896ffc321adc6aa689611adc06a85e214e807a7
david082321 <7046617+david082321@users.noreply.github.com>
2018-06-20 03:33:52 +0800

    Update aria2_old.sh

--- aria2_old.sh
+++ aria2_old.sh
@@ -16,11 +16,11 @@ aria2_log="/root/.aria2/aria2.log"
 aria2c="/usr/bin/aria2c"
 
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
-Info="${Green_font_prefix}[信息]${Font_color_suffix}"
-Error="${Red_font_prefix}[错误]${Font_color_suffix}"
+Info="${Green_font_prefix}[訊息]${Font_color_suffix}"
+Error="${Red_font_prefix}[錯誤]${Font_color_suffix}"
 Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
 
-#检查系统
+#檢查系統
 check_sys(){
 	if [[ -f /etc/redhat-release ]]; then
 		release="centos"
@@ -40,8 +40,8 @@ check_sys(){
 	bit=`uname -m`
 }
 check_installed_status(){
-	[[ ! -e ${aria2c} ]] && echo -e "${Error} Aria2 没有安装，请检查 !" && exit 1
-	[[ ! -e ${aria2_conf} ]] && echo -e "${Error} Aria2 配置文件不存在，请检查 !" && [[ $1 != "un" ]] && exit 1
+	[[ ! -e ${aria2c} ]] && echo -e "${Error} Aria2 沒有安裝，請檢查 !" && exit 1
+	[[ ! -e ${aria2_conf} ]] && echo -e "${Error} Aria2 設定檔案不存在，請檢查 !" && [[ $1 != "un" ]] && exit 1
 }
 check_pid(){
 	PID=`ps -ef| grep "aria2c"| grep -v grep| grep -v ".sh"| grep -v "init.d"| grep -v "service"| awk '{print $2}'`
@@ -49,36 +49,36 @@ check_pid(){
 Download_aria2(){
 	mkdir "${file}" && cd "${file}"
 	wget --no-check-certificate -N "https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/other/Aria2/aria2.conf"
-	[[ ! -s "aria2.conf" ]] && echo -e "${Error} Aria2 配置文件下载失败 !" && rm -rf "${file}" && exit 1
+	[[ ! -s "aria2.conf" ]] && echo -e "${Error} Aria2 設定檔案下載失敗 !" && rm -rf "${file}" && exit 1
 	wget --no-check-certificate -N "https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/other/Aria2/dht.dat"
-	[[ ! -s "dht.dat" ]] && echo -e "${Error} Aria2 DHT文件下载失败 !" && rm -rf "${file}" && exit 1
+	[[ ! -s "dht.dat" ]] && echo -e "${Error} Aria2 DHT檔案下載失敗 !" && rm -rf "${file}" && exit 1
 	echo '' > aria2.session
 }
 Service_aria2(){
 	if [[ ${release} = "centos" ]]; then
 		if ! wget --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/other/aria2_centos -O /etc/init.d/aria2; then
-			echo -e "${Error} Aria2服务 管理脚本下载失败 !" && exit 1
+			echo -e "${Error} Aria2服務 管理腳本下載失敗 !" && exit 1
 		fi
 		chmod +x /etc/init.d/aria2
 		chkconfig --add aria2
 		chkconfig aria2 on
 	else
 		if ! wget --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/other/aria2_debian -O /etc/init.d/aria2; then
-			echo -e "${Error} Aria2服务 管理脚本下载失败 !" && exit 1
+			echo -e "${Error} Aria2服務 管理腳本下載失敗 !" && exit 1
 		fi
 		chmod +x /etc/init.d/aria2
 		update-rc.d -f aria2 defaults
 	fi
-	echo -e "${Info} Aria2服务 管理脚本下载完成 !"
+	echo -e "${Info} Aria2服務 管理腳本下載完成 !"
 }
 Installation_dependency(){
 	if [[ ${release} = "centos" ]]; then
 		cat /etc/redhat-release |grep 7\..*|grep -i centos>/dev/null
 		if [[ $? = 1 ]]; then
-			echo -e "${Error} CentOS6 系统不支持安装 Aria2（源没有Aria2包），请更换 CentOS7 或其他系统安装 !" && exit 1
+			echo -e "${Error} CentOS6 系統不支援安裝 Aria2（源沒有Aria2包），請更換 CentOS7 或其他系統安裝 !" && exit 1
 		fi
 		wget -N --no-check-certificate http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-		[[ ! -e "epel-release-latest-7.noarch.rpm" ]] && echo -e "${Error} CentOS7 EPEL 源下载失败 !" && exit 1
+		[[ ! -e "epel-release-latest-7.noarch.rpm" ]] && echo -e "${Error} CentOS7 EPEL 源下載失敗 !" && exit 1
 		rpm -ivh epel-release-latest-7.noarch.rpm
 		rm -rf epel-release-latest-7.noarch.rpm
 		yum update
@@ -95,37 +95,37 @@ Installation_dependency(){
 		apt-get update
 		apt-get install aria2 curl unzip vim -y
 	fi
-	[[ ! -e ${aria2c} ]] && echo -e "${Error} Aria2 安装失败，请检查 !" && exit 1
+	[[ ! -e ${aria2c} ]] && echo -e "${Error} Aria2 安裝失敗，請檢查 !" && exit 1
 }
 Install_aria2(){
-	[[ -e ${aria2c} ]] && echo -e "${Error} Aria2 已安装，请检查 !" && exit 1
+	[[ -e ${aria2c} ]] && echo -e "${Error} Aria2 已安裝，請檢查 !" && exit 1
 	check_sys
-	echo -e "${Info} 开始安装/配置 依赖..."
+	echo -e "${Info} 開始安裝/設定 依賴..."
 	Installation_dependency
-	echo -e "${Info} 开始下载/安装 配置文件..."
+	echo -e "${Info} 開始下載/安裝 設定檔案..."
 	Download_aria2
-	echo -e "${Info} 开始下载/安装 服务脚本(init)..."
+	echo -e "${Info} 開始下載/安裝 服務腳本(init)..."
 	Service_aria2
 	Read_config
-	echo -e "${Info} 开始设置 iptables防火墙..."
+	echo -e "${Info} 開始設定 iptables防火牆..."
 	Set_iptables
-	echo -e "${Info} 开始添加 iptables防火墙规则..."
+	echo -e "${Info} 開始添加 iptables防火牆規則..."
 	Add_iptables
-	echo -e "${Info} 开始保存 iptables防火墙规则..."
+	echo -e "${Info} 開始儲存 iptables防火牆規則..."
 	Save_iptables
-	echo -e "${Info} 所有步骤 安装完毕，开始启动..."
+	echo -e "${Info} 所有步驟 安裝完畢，開始啟動..."
 	Start_aria2
 }
 Start_aria2(){
 	check_installed_status
 	check_pid
-	[[ ! -z ${PID} ]] && echo -e "${Error} Aria2 正在运行，请检查 !" && exit 1
+	[[ ! -z ${PID} ]] && echo -e "${Error} Aria2 正在執行，請檢查 !" && exit 1
 	/etc/init.d/aria2 start
 }
 Stop_aria2(){
 	check_installed_status
 	check_pid
-	[[ -z ${PID} ]] && echo -e "${Error} Aria2 没有运行，请检查 !" && exit 1
+	[[ -z ${PID} ]] && echo -e "${Error} Aria2 沒有執行，請檢查 !" && exit 1
 	/etc/init.d/aria2 stop
 }
 Restart_aria2(){
@@ -140,22 +140,22 @@ Set_aria2(){
 	Restart_aria2
 }
 Read_config(){
-	[[ ! -e ${aria2_conf} ]] && echo -e "${Error} Aria2 配置文件不存在 !" && exit 1
+	[[ ! -e ${aria2_conf} ]] && echo -e "${Error} Aria2 設定檔案不存在 !" && exit 1
 	conf_text=$(cat ${aria2_conf}|grep -v '#')
 	aria2_dir=$(echo -e "${conf_text}"|grep "dir="|awk -F "=" '{print $NF}')
 	aria2_rpc_port=$(echo -e "${conf_text}"|grep "rpc-listen-port="|awk -F "=" '{print $NF}')
 	aria2_rpc_secret=$(echo -e "${conf_text}"|grep "rpc-secret="|awk -F "=" '{print $NF}')
 }
 View_Log(){
-	[[ ! -e ${aria2_log} ]] && echo -e "${Error} Aria2 日志文件不存在 !" && exit 1
-	echo && echo -e "${Tip} 按 ${Red_font_prefix}Ctrl+C${Font_color_suffix} 终止查看日志" && echo
+	[[ ! -e ${aria2_log} ]] && echo -e "${Error} Aria2 日誌檔案不存在 !" && exit 1
+	echo && echo -e "${Tip} 按 ${Red_font_prefix}Ctrl+C${Font_color_suffix} 終止查看日誌" && echo
 	tail -f ${aria2_log}
 }
 Uninstall_aria2(){
 	check_installed_status "un"
-	echo "确定要卸载 Aria2 ? (y/N)"
+	echo "確定要移除 Aria2 ? (y/N)"
 	echo
-	stty erase '^H' && read -p "(默认: n):" unyn
+	stty erase '^H' && read -p "(預設: n):" unyn
 	[[ -z ${unyn} ]] && unyn="n"
 	if [[ ${unyn} == [Yy] ]]; then
 		check_pid
@@ -171,9 +171,9 @@ Uninstall_aria2(){
 			apt-get remove --purge aria2 -y
 		fi
 		rm -rf ${file}
-		echo && echo "Aria2 卸载完成 !" && echo
+		echo && echo "Aria2 移除完成 !" && echo
 	else
-		echo && echo "卸载已取消..." && echo
+		echo && echo "移除已取消..." && echo
 	fi
 }
 Add_iptables(){
@@ -202,50 +202,50 @@ Set_iptables(){
 	fi
 }
 Update_Shell(){
-	echo -e "当前版本为 [ ${sh_ver} ]，开始检测最新版本..."
+	echo -e "目前版本為 [ ${sh_ver} ]，開始檢測最新版本..."
 	sh_new_ver=$(wget --no-check-certificate -qO- "https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/aria2.sh"|grep 'sh_ver="'|awk -F "=" '{print $NF}'|sed 's/\"//g'|head -1)
-	[[ -z ${sh_new_ver} ]] && echo -e "${Error} 检测最新版本失败 !" && exit 0
+	[[ -z ${sh_new_ver} ]] && echo -e "${Error} 檢測最新版本失敗 !" && exit 0
 	if [[ ${sh_new_ver} != ${sh_ver} ]]; then
-		echo -e "发现新版本[ ${sh_new_ver} ]，是否更新？[Y/n]"
-		stty erase '^H' && read -p "(默认: y):" yn
+		echo -e "發現新版本[ ${sh_new_ver} ]，是否更新？[Y/n]"
+		stty erase '^H' && read -p "(預設: y):" yn
 		[[ -z "${yn}" ]] && yn="y"
 		if [[ ${yn} == [Yy] ]]; then
 			wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/aria2.sh && chmod +x aria2.sh
-			echo -e "脚本已更新为最新版本[ ${sh_new_ver} ] !"
+			echo -e "腳本已更新為最新版本[ ${sh_new_ver} ] !"
 		else
 			echo && echo "	已取消..." && echo
 		fi
 	else
-		echo -e "当前已是最新版本[ ${sh_new_ver} ] !"
+		echo -e "目前已是最新版本[ ${sh_new_ver} ] !"
 	fi
 }
-echo && echo -e " Aria2 一键安装管理脚本 ${Red_font_prefix}[v${sh_ver}]${Font_color_suffix}
+echo && echo -e " Aria2 一鍵安裝管理腳本 ${Red_font_prefix}[v${sh_ver}]${Font_color_suffix}
   -- Toyo | doub.io/shell-jc4 --
   
- ${Green_font_prefix}0.${Font_color_suffix} 升级脚本
+ ${Green_font_prefix}0.${Font_color_suffix} 升級腳本
 ————————————
- ${Green_font_prefix}1.${Font_color_suffix} 安装 Aria2
- ${Green_font_prefix}2.${Font_color_suffix} 卸载 Aria2
+ ${Green_font_prefix}1.${Font_color_suffix} 安裝 Aria2
+ ${Green_font_prefix}2.${Font_color_suffix} 移除 Aria2
 ————————————
- ${Green_font_prefix}3.${Font_color_suffix} 启动 Aria2
+ ${Green_font_prefix}3.${Font_color_suffix} 啟動 Aria2
  ${Green_font_prefix}4.${Font_color_suffix} 停止 Aria2
- ${Green_font_prefix}5.${Font_color_suffix} 重启 Aria2
+ ${Green_font_prefix}5.${Font_color_suffix} 重啟 Aria2
 ————————————
- ${Green_font_prefix}6.${Font_color_suffix} 修改 配置文件
- ${Green_font_prefix}7.${Font_color_suffix} 查看 日志信息
+ ${Green_font_prefix}6.${Font_color_suffix} 修改 設定檔案
+ ${Green_font_prefix}7.${Font_color_suffix} 查看 日誌訊息
 ————————————" && echo
 if [[ -e ${aria2c} ]]; then
 	check_pid
 	if [[ ! -z "${PID}" ]]; then
-		echo -e " 当前状态: ${Green_font_prefix}已安装${Font_color_suffix} 并 ${Green_font_prefix}已启动${Font_color_suffix}"
+		echo -e " 目前狀態: ${Green_font_prefix}已安裝${Font_color_suffix} 並 ${Green_font_prefix}已啟動${Font_color_suffix}"
 	else
-		echo -e " 当前状态: ${Green_font_prefix}已安装${Font_color_suffix} 但 ${Red_font_prefix}未启动${Font_color_suffix}"
+		echo -e " 目前狀態: ${Green_font_prefix}已安裝${Font_color_suffix} 但 ${Red_font_prefix}未啟動${Font_color_suffix}"
 	fi
 else
-	echo -e " 当前状态: ${Red_font_prefix}未安装${Font_color_suffix}"
+	echo -e " 目前狀態: ${Red_font_prefix}未安裝${Font_color_suffix}"
 fi
 echo
-stty erase '^H' && read -p " 请输入数字 [0-7]:" num
+stty erase '^H' && read -p " 請輸入數字 [0-7]:" num
 case "$num" in
 	0)
 	Update_Shell
@@ -272,6 +272,6 @@ case "$num" in
 	View_Log
 	;;
 	*)
-	echo "请输入正确数字 [0-7]"
+	echo "請輸入正確數字 [0-7]"
 	;;
 esac
