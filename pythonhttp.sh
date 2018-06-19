1423765ca76b97d10fc1d83c353e034b8a84c78d
david082321 <7046617+david082321@users.noreply.github.com>
2018-06-20 03:28:54 +0800

    Update pythonhttp.sh

--- pythonhttp.sh
+++ pythonhttp.sh
@@ -11,44 +11,44 @@ export PATH
 #=================================================
 
 sethttp(){
-#设置端口
+#設定埠
 	while true
 	do
-	echo -e "请输入要开放的HTTP服务端口 [1-65535]"
-	stty erase '^H' && read -p "(默认端口: 8000):" httpport
+	echo -e "請輸入要開放的HTTP服務埠 [1-65535]"
+	stty erase '^H' && read -p "(預設埠: 8000):" httpport
 	[[ -z "$httpport" ]] && httpport="8000"
 	expr ${httpport} + 0 &>/dev/null
 	if [[ $? -eq 0 ]]; then
 		if [[ ${httpport} -ge 1 ]] && [[ ${httpport} -le 65535 ]]; then
 			echo
-			echo -e "	端口 : \033[41;37m ${httpport} \033[0m"
+			echo -e "	埠 : \033[41;37m ${httpport} \033[0m"
 			echo
 			break
 		else
-			echo "输入错误, 请输入正确的端口。"
+			echo "輸入錯誤, 請輸入正確的埠。"
 		fi
 	else
-		echo "输入错误, 请输入正确的端口。"
+		echo "輸入錯誤, 請輸入正確的埠。"
 	fi
 	done
-	#设置目录
-	echo "请输入要开放的目录(绝对路径)"
-	stty erase '^H' && read -p "(直接回车, 默认当前文件夹):" httpfile
+	#設定目錄
+	echo "請輸入要開放的目錄(絕對路徑)"
+	stty erase '^H' && read -p "(直接回車, 預設目前資料夾):" httpfile
 	if [[ ! -z $httpfile ]]; then
-		[[ ! -e $httpfile ]] && echo -e "\033[41;37m [错误] \033[0m 输入的目录不存在 或 当前用户无权限访问, 请检查!" && exit 1
+		[[ ! -e $httpfile ]] && echo -e "\033[41;37m [錯誤] \033[0m 輸入的目錄不存在 或 目前使用者無權限訪問, 請檢查!" && exit 1
 	else
 		httpfile=`echo $PWD`
 	fi
-	#最后确认
+	#最後確認
 	echo
 	echo "========================"
-	echo "      请检查配置是否正确 !"
+	echo "      請檢查設定是否正確 !"
 	echo
-	echo -e "	端口 : \033[41;37m ${httpport} \033[0m"
-	echo -e "	目录 : \033[41;37m ${httpfile} \033[0m"
+	echo -e "	埠 : \033[41;37m ${httpport} \033[0m"
+	echo -e "	目錄 : \033[41;37m ${httpfile} \033[0m"
 	echo "========================"
 	echo
-	stty erase '^H' && read -p "按任意键继续，如有错误，请使用 Ctrl + C 退出." var
+	stty erase '^H' && read -p "按任意鍵繼續，如有錯誤，請使用 Ctrl + C 退出." var
 }
 iptables_add(){
 	iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport ${httpport} -j ACCEPT
@@ -60,7 +60,7 @@ iptables_del(){
 }
 starthttp(){
 	PID=`ps -ef | grep SimpleHTTPServer | grep -v grep | awk '{print $2}'`
-	[[ ! -z $PID ]] && echo -e "\033[41;37m [错误] \033[0m SimpleHTTPServer 正着运行，请检查 !" && exit 1
+	[[ ! -z $PID ]] && echo -e "\033[41;37m [錯誤] \033[0m SimpleHTTPServer 正著執行，請檢查 !" && exit 1
 	sethttp
 	iptables_add
 	cd ${httpfile}
@@ -68,29 +68,29 @@ starthttp(){
 	sleep 2s
 	PID=`ps -ef | grep SimpleHTTPServer | grep -v grep | awk '{print $2}'`
 	if [[ -z $PID ]]; then
-		echo -e "\033[41;37m [错误] \033[0m SimpleHTTPServer 启动失败 !" && exit 1
+		echo -e "\033[41;37m [錯誤] \033[0m SimpleHTTPServer 啟動失敗 !" && exit 1
 	else
 		ip=`curl -m 10 -s http://members.3322.org/dyndns/getip`
 		[[ -z "$ip" ]] && ip="VPS_IP"
 		echo
-		echo "HTTP服务 已启动 !"
-		echo -e "浏览器访问，地址： \033[41;37m http://${ip}:${httpport} \033[0m "
+		echo "HTTP服務 已啟動 !"
+		echo -e "瀏覽器訪問，地址： \033[41;37m http://${ip}:${httpport} \033[0m "
 		echo
 	fi
 }
 stophttp(){
 	PID=`ps -ef | grep SimpleHTTPServer | grep -v grep | awk '{print $2}'`
-	[[ -z $PID ]] && echo -e "\033[41;37m [错误] \033[0m 没有发现 SimpleHTTPServer 进程运行，请检查 !" && exit 1
+	[[ -z $PID ]] && echo -e "\033[41;37m [錯誤] \033[0m 沒有發現 SimpleHTTPServer 進程執行，請檢查 !" && exit 1
 	port=`netstat -lntp | grep ${PID} | awk '{print $4}' | awk -F ":" '{print $2}'`
 	iptables_del
 	kill -9 ${PID}
 	sleep 2s
 	PID=`ps -ef | grep SimpleHTTPServer | grep -v grep | awk '{print $2}'`
 	if [[ ! -z $PID ]]; then
-		echo -e "\033[41;37m [错误] \033[0m SimpleHTTPServer 停止失败 !" && exit 1
+		echo -e "\033[41;37m [錯誤] \033[0m SimpleHTTPServer 停止失敗 !" && exit 1
 	else
 		echo
-		echo "HTTP服务 已停止 !"
+		echo "HTTP服務 已停止 !"
 		echo
 	fi
 }
@@ -102,7 +102,7 @@ case "$action" in
     ${action}http
     ;;
     *)
-    echo "输入错误 !"
+    echo "輸入錯誤 !"
     echo "用法: {start|stop}"
     ;;
 esac
