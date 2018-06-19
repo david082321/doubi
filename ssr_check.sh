846836c2992f1f0f7938794e83646df09bbfc928
david082321 <7046617+david082321@users.noreply.github.com>
2018-06-20 03:27:03 +0800

    Update ssr_check.sh

--- ssr_check.sh
+++ ssr_check.sh
@@ -16,43 +16,43 @@ log_file="$PWD/ssr_check.log"
 config_file="$PWD/ssr_check.conf"
 
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
-Info="${Green_font_prefix}[信息]${Font_color_suffix}" && Error="${Red_font_prefix}[错误]${Font_color_suffix}" && Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
+Info="${Green_font_prefix}[訊息]${Font_color_suffix}" && Error="${Red_font_prefix}[錯誤]${Font_color_suffix}" && Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
 
 set_config_ip(){
-	echo "请输入 ShadowsocksR 账号服务器公网IP"
-	stty erase '^H' && read -p "(默认取消):" ip
+	echo "請輸入 ShadowsocksR 帳號伺服器公網IP"
+	stty erase '^H' && read -p "(預設取消):" ip
 	[[ -z "${ip}" ]] && echo "已取消..." && exit 1
 	echo && echo -e "	I   P : ${Red_font_prefix}${ip}${Font_color_suffix}" && echo
 }
 set_config_port(){
 	while true
 	do
-	echo -e "请输入 ShadowsocksR 账号端口"
-	stty erase '^H' && read -p "(默认: 2333):" port
+	echo -e "請輸入 ShadowsocksR 帳號埠"
+	stty erase '^H' && read -p "(預設: 2333):" port
 	[[ -z "$port" ]] && port="2333"
 	expr ${port} + 0 &>/dev/null
 	if [[ $? -eq 0 ]]; then
 		if [[ ${port} -ge 1 ]] && [[ ${port} -le 65535 ]]; then
-			echo && echo -e "	端口 : ${Red_font_prefix}${port}${Font_color_suffix}" && echo
+			echo && echo -e "	埠 : ${Red_font_prefix}${port}${Font_color_suffix}" && echo
 			break
 		else
-			echo -e "${Error} 请输入正确的数字！"
+			echo -e "${Error} 請輸入正確的數位！"
 		fi
 	else
-		echo -e "${Error} 请输入正确的数字！"
+		echo -e "${Error} 請輸入正確的數位！"
 	fi
 	done
 }
 set_config_password(){
-	echo "请输入 ShadowsocksR 账号密码"
-	stty erase '^H' && read -p "(默认: doub.io):" passwd
+	echo "請輸入 ShadowsocksR 帳號密碼"
+	stty erase '^H' && read -p "(預設: doub.io):" passwd
 	[[ -z "${passwd}" ]] && passwd="doub.io"
-	echo && echo -e "	密码 : ${Red_font_prefix}${passwd}${Font_color_suffix}" && echo
+	echo && echo -e "	密碼 : ${Red_font_prefix}${passwd}${Font_color_suffix}" && echo
 }
 set_config_method(){
-	echo -e "请选择要设置的ShadowsocksR账号 加密方式
+	echo -e "請選擇要設置的ShadowsocksR帳號 加密方式
  ${Green_font_prefix} 1.${Font_color_suffix} none
- ${Tip} 如果使用 auth_chain_a 协议，请加密方式选择 none，混淆随意(建议 plain)
+ ${Tip} 如果使用 auth_chain_a 協議，請加密方式選擇 none，混淆隨意(建議 plain)
  
  ${Green_font_prefix} 2.${Font_color_suffix} rc4
  ${Green_font_prefix} 3.${Font_color_suffix} rc4-md5
@@ -73,8 +73,8 @@ set_config_method(){
  ${Green_font_prefix}14.${Font_color_suffix} salsa20
  ${Green_font_prefix}15.${Font_color_suffix} chacha20
  ${Green_font_prefix}16.${Font_color_suffix} chacha20-ietf
- ${Tip} salsa20/chacha20-*系列加密方式，需要额外安装依赖 libsodium ，否则会无法启动ShadowsocksR !" && echo
-	stty erase '^H' && read -p "(默认: 5. aes-128-ctr):" method
+ ${Tip} salsa20/chacha20-*系列加密方式，需要額外安裝依賴 libsodium ，否則會無法啟動ShadowsocksR !" && echo
+	stty erase '^H' && read -p "(預設: 5. aes-128-ctr):" method
 	[[ -z "${method}" ]] && method="5"
 	if [[ ${method} == "1" ]]; then
 		method="none"
@@ -114,14 +114,14 @@ set_config_method(){
 	echo && echo ${Separator_1} && echo -e "	加密 : ${Red_font_prefix}${method}${Font_color_suffix}" && echo ${Separator_1} && echo
 }
 set_config_protocol(){
-	echo -e "请选择要设置的ShadowsocksR账号 协议插件
+	echo -e "請選擇要設置的ShadowsocksR帳號 協議插件
  ${Green_font_prefix}1.${Font_color_suffix} origin
  ${Green_font_prefix}2.${Font_color_suffix} auth_sha1_v4
  ${Green_font_prefix}3.${Font_color_suffix} auth_aes128_md5
  ${Green_font_prefix}4.${Font_color_suffix} auth_aes128_sha1
  ${Green_font_prefix}5.${Font_color_suffix} auth_chain_a
- ${Tip} 如果使用 auth_chain_a 协议，请加密方式选择 none，混淆随意(建议 plain)" && echo
-	stty erase '^H' && read -p "(默认: 2. auth_sha1_v4):" protocol
+ ${Tip} 如果使用 auth_chain_a 協議，請加密方式選擇 none，混淆隨意(建議 plain)" && echo
+	stty erase '^H' && read -p "(預設: 2. auth_sha1_v4):" protocol
 	[[ -z "${protocol}" ]] && protocol="2"
 	if [[ ${protocol} == "1" ]]; then
 		protocol="origin"
@@ -136,17 +136,17 @@ set_config_protocol(){
 	else
 		protocol="auth_sha1_v4"
 	fi
-	echo && echo -e "	协议 : ${Red_font_prefix}${protocol}${Font_color_suffix}" && echo
+	echo && echo -e "	協議 : ${Red_font_prefix}${protocol}${Font_color_suffix}" && echo
 }
 set_config_obfs(){
-	echo -e "请选择要设置的ShadowsocksR账号 混淆插件
+	echo -e "請選擇要設置的ShadowsocksR帳號 混淆插件
  ${Green_font_prefix}1.${Font_color_suffix} plain
  ${Green_font_prefix}2.${Font_color_suffix} http_simple
  ${Green_font_prefix}3.${Font_color_suffix} http_post
  ${Green_font_prefix}4.${Font_color_suffix} random_head
  ${Green_font_prefix}5.${Font_color_suffix} tls1.2_ticket_auth
- ${Tip} 如果使用 ShadowsocksR 加速游戏，请选择 混淆兼容原版或 plain 混淆，然后客户端选择 plain，否则会增加延迟 !" && echo
-	stty erase '^H' && read -p "(默认: 5. tls1.2_ticket_auth):" obfs
+ ${Tip} 如果使用 ShadowsocksR 加速遊戲，請選擇 混淆相容原版或 plain 混淆，然後使用者端選擇 plain，否則會增加延遲 !" && echo
+	stty erase '^H' && read -p "(預設: 5. tls1.2_ticket_auth):" obfs
 	[[ -z "${obfs}" ]] && obfs="5"
 	if [[ ${obfs} == "1" ]]; then
 		obfs="plain"
@@ -164,19 +164,19 @@ set_config_obfs(){
 	echo && echo -e "	混淆 : ${Red_font_prefix}${obfs}${Font_color_suffix}" && echo
 }
 set_config_like(){
-	echo "请输入 ShadowsocksR 的链接(SS/SSR链接皆可，如 ss://xxxx ssr://xxxx)"
-	stty erase '^H' && read -p "(默认回车取消):" Like
+	echo "請輸入 ShadowsocksR 的連結(SS/SSR連結皆可，如 ss://xxxx ssr://xxxx)"
+	stty erase '^H' && read -p "(預設回車取消):" Like
 	[[ -z "${Like}" ]] && echo "已取消..." && exit 1
-	echo && echo -e "	链接 : ${Red_font_prefix}${Like}${Font_color_suffix}" && echo
+	echo && echo -e "	連結 : ${Red_font_prefix}${Like}${Font_color_suffix}" && echo
 }
 set_config_user(){
-	echo -e "请输入选择输入方式
- ${Green_font_prefix}1.${Font_color_suffix} 输入ShadowsocksR账号全部信息(Shadowsocks原版也可以)
- ${Green_font_prefix}2.${Font_color_suffix} 输入ShadowsocksR账号的 SSR链接(Shadowsocks原版也可以)"
-	stty erase '^H' && read -p "(默认:2):" enter_type
+	echo -e "請輸入選擇輸入方式
+ ${Green_font_prefix}1.${Font_color_suffix} 輸入ShadowsocksR帳號全部訊息(Shadowsocks原版也可以)
+ ${Green_font_prefix}2.${Font_color_suffix} 輸入ShadowsocksR帳號的 SSR連結(Shadowsocks原版也可以)"
+	stty erase '^H' && read -p "(預設:2):" enter_type
 	[[ -z "${enter_type}" ]] && enter_type="2"
 	if [[ ${enter_type} == "1" ]]; then
-		echo -e "下面依次开始输入要检测可用性的 ShadowsocksR账号信息。" && echo
+		echo -e "下面依次開始輸入要檢測可用性的 ShadowsocksR帳號訊息。" && echo
 		set_config_ip
 		set_config_port
 		set_config_password
@@ -193,16 +193,16 @@ set_config_user(){
 	fi
 }
 GO(){
-	echo -e "========== 开始记录测试信息 [$(date '+%Y-%m-%d %H:%M:%S')]==========\n" >> ${log_file}
+	echo -e "========== 開始記錄測試訊息 [$(date '+%Y-%m-%d %H:%M:%S')]==========\n" >> ${log_file}
 }
 exit_GG(){
-	echo -e "========== 记录测试信息结束 [$(date '+%Y-%m-%d %H:%M:%S')]==========\n\n" >> ${log_file}
+	echo -e "========== 記錄測試訊息結束 [$(date '+%Y-%m-%d %H:%M:%S')]==========\n\n" >> ${log_file}
 	exit 0
 }
 Get_Like(){
-	[[ ! -e ${config_file} ]] && echo -e "${Error} 配置文件不存在！(${config_file})" | tee -a ${log_file} && exit_GG
+	[[ ! -e ${config_file} ]] && echo -e "${Error} 設定檔案不存在！(${config_file})" | tee -a ${log_file} && exit_GG
 	Like=$(cat "${config_file}")
-	[[ -z ${Like} ]] && echo -e "${Error} 获取SS/SSR账号信息失败或配置文件为空 !" | tee -a ${log_file} && exit_GG
+	[[ -z ${Like} ]] && echo -e "${Error} 獲取SS/SSR帳號訊息失敗或設定檔案為空 !" | tee -a ${log_file} && exit_GG
 	Like_num=$(echo -e "${Like}"|wc -l)
 }
 Analysis_Config(){
@@ -211,7 +211,7 @@ Analysis_Config(){
 	if [[ "${Config_info_base64_determine}" == "ssr://" ]]; then
 		Config_info=$(echo -e "${Config_info_base64}"|cut -c 7-2000|base64 -d)
 		if [[ -z ${Config_info} ]]; then
-			echo -e "${Error} Base64解密失败 [${Config_info_base64}] !" | tee -a ${log_file}
+			echo -e "${Error} Base64解密失敗 [${Config_info_base64}] !" | tee -a ${log_file}
 			if [[ ${analysis_type} == "add" ]]; then
 				exit_GG
 			else
@@ -222,7 +222,7 @@ Analysis_Config(){
 	else
 		Config_info=$(echo -e "${Config_info_base64}"|cut -c 6-2000|base64 -d)
 		if [[ -z ${Config_info} ]]; then
-			echo -e "${Error} Base64解密失败 [${Config_info_base64}] !" | tee -a ${log_file}
+			echo -e "${Error} Base64解密失敗 [${Config_info_base64}] !" | tee -a ${log_file}
 			if [[ ${analysis_type} == "add" ]]; then
 				exit_GG
 			else
@@ -238,7 +238,7 @@ ss_config(){
 	port=$(echo -e "${you}"|awk -F ":" '{print $NF}')
 	ip=$(echo -e "${you}"|awk -F ":${port}" '{print $1}')
 	if [[ $(echo -e "${ip}"|wc -L) -le 8 ]]; then
-		echo -e "${Error} 错误，IP格式错误或为 ipv6地址[ ${ip} ]" | tee -a ${log_file}
+		echo -e "${Error} 錯誤，IP格式錯誤或為 ipv6地址[ ${ip} ]" | tee -a ${log_file}
 		if [[ ${analysis_type} == "add" ]]; then
 			exit_GG
 		else
@@ -250,7 +250,7 @@ ss_config(){
 	protocol="origin"
 	obfs="plain"
 	if [[ -z ${ip} ]] || [[ -z ${port} ]] || [[ -z ${method} ]] || [[ -z ${passwd} ]] || [[ -z ${protocol} ]] || [[ -z ${obfs} ]]; then
-		echo -e "${Error} 错误，有部分 账号参数为空！[ ${ip} ,${port} ,${method} ,${passwd} ,${protocol} ,${obfs} ]" | tee -a ${log_file}
+		echo -e "${Error} 錯誤，有部分 帳號參數為空！[ ${ip} ,${port} ,${method} ,${passwd} ,${protocol} ,${obfs} ]" | tee -a ${log_file}
 		if [[ ${analysis_type} == "add" ]]; then
 			exit_GG
 		else
@@ -271,7 +271,7 @@ ssr_config(){
 	port=$(echo -e "${zuo}"|awk -F ":" '{print $NF}')
 	ip=$(echo -e "${Config_info}"|awk -F ":${port}" '{print $1}')
 	if [[ $(echo -e "${ip}"|wc -L) -le 8 ]]; then
-		echo -e "${Error} 错误，IP格式错误[ ${ip} ]" | tee -a ${log_file}
+		echo -e "${Error} 錯誤，IP格式錯誤[ ${ip} ]" | tee -a ${log_file}
 		if [[ ${analysis_type} == "add" ]]; then
 			exit_GG
 		else
@@ -280,7 +280,7 @@ ssr_config(){
 	fi
 	passwd=$(echo -e "${passwd_base64}"|base64 -d)
 	if [[ -z ${ip} ]] || [[ -z ${port} ]] || [[ -z ${method} ]] || [[ -z ${passwd} ]] || [[ -z ${protocol} ]] || [[ -z ${obfs} ]]; then
-		echo -e "${Error} 错误，有部分 账号参数为空！[ ${ip} ,${port} ,${method} ,${passwd} ,${protocol} ,${obfs} ]" | tee -a ${log_file}
+		echo -e "${Error} 錯誤，有部分 帳號參數為空！[ ${ip} ,${port} ,${method} ,${passwd} ,${protocol} ,${obfs} ]" | tee -a ${log_file}
 		if [[ ${analysis_type} == "add" ]]; then
 			exit_GG
 		else
@@ -293,7 +293,7 @@ Start_Client(){
 	sleep 2s
 	PID=$(ps -ef |grep -v grep | grep "local.py" | grep "${local_port}" |awk '{print $2}')
 	if [[ -z ${PID} ]]; then
-		echo -e "${Error} ShadowsocksR客户端 启动失败，请检查 !" | tee -a ${log_file}
+		echo -e "${Error} ShadowsocksR使用者端 啟動失敗，請檢查 !" | tee -a ${log_file}
 		if [[ ${analysis_type} == "add" ]]; then
 			exit_GG
 		else
@@ -304,16 +304,16 @@ Start_Client(){
 Socks5_test(){
 	Test_results=$(curl --socks5 127.0.0.1:${local_port} -k -m ${Timeout} -s "${Test_URL}")
 	if [[ -z ${Test_results} ]]; then
-		echo -e "${Error} [${ip}] 检测失败，账号不可用 !" | tee -a ${log_file}
+		echo -e "${Error} [${ip}] 檢測失敗，帳號不可用 !" | tee -a ${log_file}
 		Config_Status="false"
 	else
-		echo -e "${Info} [${ip}] 检测成功，账号可用 !" | tee -a ${log_file}
+		echo -e "${Info} [${ip}] 檢測成功，帳號可用 !" | tee -a ${log_file}
 		Config_Status="true"
 	fi
 	kill -9 ${PID}
 	PID=$(ps -ef |grep -v grep | grep "local.py" | grep "${local_port}" |awk '{print $2}')
 	if [[ ! -z ${PID} ]]; then
-		echo -e "${Error} ShadowsocksR客户端 停止失败，请检查 !" | tee -a ${log_file}
+		echo -e "${Error} ShadowsocksR使用者端 停止失敗，請檢查 !" | tee -a ${log_file}
 		if [[ ${analysis_type} == "add" ]]; then
 			exit_GG
 		else
@@ -352,7 +352,7 @@ Test_add(){
 	exit_GG
 }
 View_log(){
-	[[ ! -e ${log_file} ]] && echo -e "${Error} 找不到 日志文件！(${log_file})"
+	[[ ! -e ${log_file} ]] && echo -e "${Error} 找不到 日誌檔案！(${log_file})"
 	cat "${log_file}"
 }
 action=$1
