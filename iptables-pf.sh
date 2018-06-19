78aa8e4832a2d6b6a4248f3b5793e4ba006ac5c4
david082321 <7046617+david082321@users.noreply.github.com>
2018-06-20 03:30:57 +0800

    Update iptables-pf.sh

--- iptables-pf.sh
+++ iptables-pf.sh
@@ -12,13 +12,13 @@ export PATH
 sh_ver="1.1.1"
 
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
-Info="${Green_font_prefix}[信息]${Font_color_suffix}"
-Error="${Red_font_prefix}[错误]${Font_color_suffix}"
+Info="${Green_font_prefix}[訊息]${Font_color_suffix}"
+Error="${Red_font_prefix}[錯誤]${Font_color_suffix}"
 Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
 
 check_iptables(){
 	iptables_exist=$(iptables -V)
-	[[ ${iptables_exist} = "" ]] && echo -e "${Error} 没有安装iptables，请检查 !" && exit 1
+	[[ ${iptables_exist} = "" ]] && echo -e "${Error} 沒有安裝iptables，請檢查 !" && exit 1
 }
 check_sys(){
 	if [[ -f /etc/redhat-release ]]; then
@@ -41,9 +41,9 @@ check_sys(){
 install_iptables(){
 	iptables_exist=$(iptables -V)
 	if [[ ${iptables_exist} != "" ]]; then
-		echo -e "${Info} 已经安装iptables，继续..."
+		echo -e "${Info} 已經安裝iptables，繼續..."
 	else
-		echo -e "${Info} 检测到未安装 iptables，开始安装..."
+		echo -e "${Info} 檢測到未安裝 iptables，開始安裝..."
 		if [[ ${release}  == "centos" ]]; then
 			yum update
 			yum install -y vim iptables
@@ -53,49 +53,49 @@ install_iptables(){
 		fi
 		iptables_exist=$(iptables -V)
 		if [[ ${iptables_exist} = "" ]]; then
-			echo -e "${Error} 安装iptables失败，请检查 !" && exit 1
+			echo -e "${Error} 安裝iptables失敗，請檢查 !" && exit 1
 		else
-			echo -e "${Info} iptables 安装完成 !"
+			echo -e "${Info} iptables 安裝完成 !"
 		fi
 	fi
-	echo -e "${Info} 开始配置 iptables !"
+	echo -e "${Info} 開始設定 iptables !"
 	Set_iptables
-	echo -e "${Info} iptables 配置完毕 !"
+	echo -e "${Info} iptables 設定完畢 !"
 }
 Set_forwarding_port(){
-	stty erase '^H' && read -p "请输入 iptables 欲转发至的 远程端口 [1-65535] (支持端口段 如 2333-6666, 被转发服务器):" forwarding_port
+	stty erase '^H' && read -p "請輸入 iptables 欲轉發至的 遠程埠 [1-65535] (支援埠段 如 2333-6666, 被轉發伺服器):" forwarding_port
 	[[ -z "${forwarding_port}" ]] && echo "取消..." && exit 1
-	echo && echo -e "	欲转发端口 : ${Red_font_prefix}${forwarding_port}${Font_color_suffix}" && echo
+	echo && echo -e "	欲轉發埠 : ${Red_font_prefix}${forwarding_port}${Font_color_suffix}" && echo
 }
 Set_forwarding_ip(){
-		stty erase '^H' && read -p "请输入 iptables 欲转发至的 远程IP(被转发服务器):" forwarding_ip
+		stty erase '^H' && read -p "請輸入 iptables 欲轉發至的 遠程IP(被轉發伺服器):" forwarding_ip
 		[[ -z "${forwarding_ip}" ]] && echo "取消..." && exit 1
-		echo && echo -e "	欲转发服务器IP : ${Red_font_prefix}${forwarding_ip}${Font_color_suffix}" && echo
+		echo && echo -e "	欲轉發伺服器IP : ${Red_font_prefix}${forwarding_ip}${Font_color_suffix}" && echo
 }
 Set_local_port(){
-	echo -e "请输入 iptables 本地监听端口 [1-65535] (支持端口段 如 2333-6666)"
-	stty erase '^H' && read -p "(默认端口: ${forwarding_port}):" local_port
+	echo -e "請輸入 iptables 本機監聽埠 [1-65535] (支援埠段 如 2333-6666)"
+	stty erase '^H' && read -p "(預設埠: ${forwarding_port}):" local_port
 	[[ -z "${local_port}" ]] && local_port="${forwarding_port}"
-	echo && echo -e "	本地监听端口 : ${Red_font_prefix}${local_port}${Font_color_suffix}" && echo
+	echo && echo -e "	本機監聽埠 : ${Red_font_prefix}${local_port}${Font_color_suffix}" && echo
 }
 Set_local_ip(){
-	stty erase '^H' && read -p "请输入 本服务器的 网卡IP(注意是网卡绑定的IP，而不仅仅是公网IP，回车自动检测外网IP):" local_ip
+	stty erase '^H' && read -p "請輸入 本伺服器的 網卡IP(注意是網卡綁定的IP，而不僅僅是公網IP，回車自動檢測外網IP):" local_ip
 	if [[ -z "${local_ip}" ]]; then
 		local_ip=$(wget -qO- -t1 -T2 ipinfo.io/ip)
 		if [[ -z "${local_ip}" ]]; then
-			echo "${Error} 无法检测到本服务器的公网IP，请手动输入"
-			stty erase '^H' && read -p "请输入 本服务器的 网卡IP(注意是网卡绑定的IP，而不仅仅是公网IP):" local_ip
+			echo "${Error} 無法檢測到本伺服器的公網IP，請手動輸入"
+			stty erase '^H' && read -p "請輸入 本伺服器的 網卡IP(注意是網卡綁定的IP，而不僅僅是公網IP):" local_ip
 			[[ -z "${local_ip}" ]] && echo "取消..." && exit 1
 		fi
 	fi
-	echo && echo -e "	本服务器IP : ${Red_font_prefix}${local_ip}${Font_color_suffix}" && echo
+	echo && echo -e "	本伺服器IP : ${Red_font_prefix}${local_ip}${Font_color_suffix}" && echo
 }
 Set_forwarding_type(){
-	echo -e "请输入数字 来选择 iptables 转发类型:
+	echo -e "請輸入數字 來選擇 iptables 轉發類型:
  1. TCP
  2. UDP
  3. TCP+UDP\n"
-	stty erase '^H' && read -p "(默认: TCP+UDP):" forwarding_type_num
+	stty erase '^H' && read -p "(預設: TCP+UDP):" forwarding_type_num
 	[[ -z "${forwarding_type_num}" ]] && forwarding_type_num="3"
 	if [[ ${forwarding_type_num} == "1" ]]; then
 		forwarding_type="TCP"
@@ -114,14 +114,14 @@ Set_Config(){
 	Set_local_ip
 	Set_forwarding_type
 	echo && echo -e "——————————————————————————————
-	请检查 iptables 端口转发规则配置是否有误 !\n
-	本地监听端口    : ${Green_font_prefix}${local_port}${Font_color_suffix}
-	服务器 IP\t: ${Green_font_prefix}${local_ip}${Font_color_suffix}\n
-	欲转发的端口    : ${Green_font_prefix}${forwarding_port}${Font_color_suffix}
-	欲转发 IP\t: ${Green_font_prefix}${forwarding_ip}${Font_color_suffix}
-	转发类型\t: ${Green_font_prefix}${forwarding_type}${Font_color_suffix}
+	請檢查 iptables 埠轉發規則設定是否有誤 !\n
+	本機監聽埠    : ${Green_font_prefix}${local_port}${Font_color_suffix}
+	伺服器 IP\t: ${Green_font_prefix}${local_ip}${Font_color_suffix}\n
+	欲轉發的埠    : ${Green_font_prefix}${forwarding_port}${Font_color_suffix}
+	欲轉發 IP\t: ${Green_font_prefix}${forwarding_ip}${Font_color_suffix}
+	轉發類型\t: ${Green_font_prefix}${forwarding_type}${Font_color_suffix}
 ——————————————————————————————\n"
-	stty erase '^H' && read -p "请按任意键继续，如有配置错误请使用 Ctrl+C 退出。" var
+	stty erase '^H' && read -p "請按任意鍵繼續，如有設定錯誤請使用 Ctrl+C 退出。" var
 }
 Add_forwarding(){
 	check_iptables
@@ -138,18 +138,18 @@ Add_forwarding(){
 	fi
 	Save_iptables
 	clear && echo && echo -e "——————————————————————————————
-	iptables 端口转发规则配置完成 !\n
-	本地监听端口    : ${Green_font_prefix}${local_port}${Font_color_suffix}
-	服务器 IP\t: ${Green_font_prefix}${local_ip}${Font_color_suffix}\n
-	欲转发的端口    : ${Green_font_prefix}${forwarding_port_1}${Font_color_suffix}
-	欲转发 IP\t: ${Green_font_prefix}${forwarding_ip}${Font_color_suffix}
-	转发类型\t: ${Green_font_prefix}${forwarding_type}${Font_color_suffix}
+	iptables 埠轉發規則設定完成 !\n
+	本機監聽埠    : ${Green_font_prefix}${local_port}${Font_color_suffix}
+	伺服器 IP\t: ${Green_font_prefix}${local_ip}${Font_color_suffix}\n
+	欲轉發的埠    : ${Green_font_prefix}${forwarding_port_1}${Font_color_suffix}
+	欲轉發 IP\t: ${Green_font_prefix}${forwarding_ip}${Font_color_suffix}
+	轉發類型\t: ${Green_font_prefix}${forwarding_type}${Font_color_suffix}
 ——————————————————————————————\n"
 }
 View_forwarding(){
 	check_iptables
 	forwarding_text=$(iptables -t nat -vnL PREROUTING|tail -n +3)
-	[[ -z ${forwarding_text} ]] && echo -e "${Error} 没有发现 iptables 端口转发规则，请检查 !" && exit 1
+	[[ -z ${forwarding_text} ]] && echo -e "${Error} 沒有發現 iptables 埠轉發規則，請檢查 !" && exit 1
 	forwarding_total=$(echo -e "${forwarding_text}"|wc -l)
 	forwarding_list_all=""
 	for((integer = 1; integer <= ${forwarding_total}; integer++))
@@ -158,9 +158,9 @@ View_forwarding(){
 		forwarding_listen=$(echo -e "${forwarding_text}"|awk '{print $11}'|sed -n "${integer}p"|awk -F "dpt:" '{print $2}')
 		[[ -z ${forwarding_listen} ]] && forwarding_listen=$(echo -e "${forwarding_text}"| awk '{print $11}'|sed -n "${integer}p"|awk -F "dpts:" '{print $2}')
 		forwarding_fork=$(echo -e "${forwarding_text}"| awk '{print $12}'|sed -n "${integer}p"|awk -F "to:" '{print $2}')
-		forwarding_list_all=${forwarding_list_all}"${Green_font_prefix}"${integer}".${Font_color_suffix} 类型: ${Green_font_prefix}"${forwarding_type}"${Font_color_suffix} 监听端口: ${Red_font_prefix}"${forwarding_listen}"${Font_color_suffix} 转发IP和端口: ${Red_font_prefix}"${forwarding_fork}"${Font_color_suffix}\n"
+		forwarding_list_all=${forwarding_list_all}"${Green_font_prefix}"${integer}".${Font_color_suffix} 類型: ${Green_font_prefix}"${forwarding_type}"${Font_color_suffix} 監聽埠: ${Red_font_prefix}"${forwarding_listen}"${Font_color_suffix} 轉發IP和埠: ${Red_font_prefix}"${forwarding_fork}"${Font_color_suffix}\n"
 	done
-	echo && echo -e "当前有 ${Green_background_prefix} "${forwarding_total}" ${Font_color_suffix} 个 iptables 端口转发规则。"
+	echo && echo -e "目前有 ${Green_background_prefix} "${forwarding_total}" ${Font_color_suffix} 個 iptables 埠轉發規則。"
 	echo -e ${forwarding_list_all}
 }
 Del_forwarding(){
@@ -168,7 +168,7 @@ Del_forwarding(){
 	while true
 	do
 	View_forwarding
-	stty erase '^H' && read -p "请输入数字 来选择要删除的 iptables 端口转发规则(默认回车取消):" Del_forwarding_num
+	stty erase '^H' && read -p "請輸入數字 來選擇要刪除的 iptables 埠轉發規則(預設回車取消):" Del_forwarding_num
 	[[ -z "${Del_forwarding_num}" ]] && Del_forwarding_num="0"
 	expr ${Del_forwarding_num} + 0 &>/dev/null
 	if [[ $? -eq 0 ]]; then
@@ -178,9 +178,9 @@ Del_forwarding(){
 			[[ -z ${forwarding_listen} ]] && forwarding_listen=$(echo -e "${forwarding_text}"| awk '{print $11}' |sed -n "${Del_forwarding_num}p" | awk -F "dpts:" '{print $2}')
 			Del_iptables "${forwarding_type}" "${Del_forwarding_num}"
 			Save_iptables
-			echo && echo -e "${Info} iptables 端口转发规则删除完成 !" && echo
+			echo && echo -e "${Info} iptables 埠轉發規則刪除完成 !" && echo
 		else
-			echo -e "${Error} 请输入正确的数字 !"
+			echo -e "${Error} 請輸入正確的數位 !"
 		fi
 	else
 		break && echo "取消..."
@@ -189,12 +189,12 @@ Del_forwarding(){
 }
 Uninstall_forwarding(){
 	check_iptables
-	echo -e "确定要清空 iptables 所有端口转发规则 ? [y/N]"
-	stty erase '^H' && read -p "(默认: n):" unyn
+	echo -e "確定要清空 iptables 所有埠轉發規則 ? [y/N]"
+	stty erase '^H' && read -p "(預設: n):" unyn
 	[[ -z ${unyn} ]] && unyn="n"
 	if [[ ${unyn} == [Yy] ]]; then
 		forwarding_text=$(iptables -t nat -vnL PREROUTING|tail -n +3)
-		[[ -z ${forwarding_text} ]] && echo -e "${Error} 没有发现 iptables 端口转发规则，请检查 !" && exit 1
+		[[ -z ${forwarding_text} ]] && echo -e "${Error} 沒有發現 iptables 埠轉發規則，請檢查 !" && exit 1
 		forwarding_total=$(echo -e "${forwarding_text}"|wc -l)
 		for((integer = 1; integer <= ${forwarding_total}; integer++))
 		do
@@ -205,7 +205,7 @@ Uninstall_forwarding(){
 			Del_iptables "${forwarding_type}" "${integer}"
 		done
 		Save_iptables
-		echo && echo -e "${Info} iptables 已清空 所有端口转发规则 !" && echo
+		echo && echo -e "${Info} iptables 已清空 所有埠轉發規則 !" && echo
 	else
 		echo && echo "清空已取消..." && echo
 	fi
@@ -243,38 +243,38 @@ Set_iptables(){
 	fi
 }
 Update_Shell(){
-	echo -e "当前版本为 [ ${sh_ver} ]，开始检测最新版本..."
+	echo -e "目前版本為 [ ${sh_ver} ]，開始檢測最新版本..."
 	sh_new_ver=$(wget --no-check-certificate -qO- "https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/iptables-pf.sh"|grep 'sh_ver="'|awk -F "=" '{print $NF}'|sed 's/\"//g'|head -1)
-	[[ -z ${sh_new_ver} ]] && echo -e "${Error} 检测最新版本失败 !" && exit 1
+	[[ -z ${sh_new_ver} ]] && echo -e "${Error} 檢測最新版本失敗 !" && exit 1
 	if [[ ${sh_new_ver} != ${sh_ver} ]]; then
-		echo -e "发现新版本[ ${sh_new_ver} ]，是否更新？[Y/n]"
-		stty erase '^H' && read -p "(默认: y):" yn
+		echo -e "發現新版本[ ${sh_new_ver} ]，是否更新？[Y/n]"
+		stty erase '^H' && read -p "(預設: y):" yn
 		[[ -z "${yn}" ]] && yn="y"
 		if [[ ${yn} == [Yy] ]]; then
 			wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/iptables-pf.sh && chmod +x iptables-pf.sh
-			echo -e "脚本已更新为最新版本[ ${sh_new_ver} ] !"
+			echo -e "腳本已更新為最新版本[ ${sh_new_ver} ] !"
 		else
 			echo && echo "	已取消..." && echo
 		fi
 	else
-		echo -e "当前已是最新版本[ ${sh_new_ver} ] !"
+		echo -e "目前已是最新版本[ ${sh_new_ver} ] !"
 	fi
 }
 check_sys
-echo && echo -e " iptables 端口转发一键管理脚本 ${Red_font_prefix}[v${sh_ver}]${Font_color_suffix}
+echo && echo -e " iptables 埠轉發一鍵管理腳本 ${Red_font_prefix}[v${sh_ver}]${Font_color_suffix}
   -- Toyo | doub.io/wlzy-20 --
   
- ${Green_font_prefix}0.${Font_color_suffix} 升级脚本
+ ${Green_font_prefix}0.${Font_color_suffix} 升級腳本
 ————————————
- ${Green_font_prefix}1.${Font_color_suffix} 安装 iptables
- ${Green_font_prefix}2.${Font_color_suffix} 清空 iptables 端口转发
+ ${Green_font_prefix}1.${Font_color_suffix} 安裝 iptables
+ ${Green_font_prefix}2.${Font_color_suffix} 清空 iptables 埠轉發
 ————————————
- ${Green_font_prefix}3.${Font_color_suffix} 查看 iptables 端口转发
- ${Green_font_prefix}4.${Font_color_suffix} 添加 iptables 端口转发
- ${Green_font_prefix}5.${Font_color_suffix} 删除 iptables 端口转发
+ ${Green_font_prefix}3.${Font_color_suffix} 查看 iptables 埠轉發
+ ${Green_font_prefix}4.${Font_color_suffix} 添加 iptables 埠轉發
+ ${Green_font_prefix}5.${Font_color_suffix} 刪除 iptables 埠轉發
 ————————————
-注意：初次使用前请请务必执行 ${Green_font_prefix}1. 安装 iptables${Font_color_suffix}(不仅仅是安装)" && echo
-stty erase '^H' && read -p " 请输入数字 [0-5]:" num
+注意：初次使用前請請務必執行 ${Green_font_prefix}1. 安裝 iptables${Font_color_suffix}(不僅僅是安裝)" && echo
+stty erase '^H' && read -p " 請輸入數字 [0-5]:" num
 case "$num" in
 	0)
 	Update_Shell
@@ -295,6 +295,6 @@ case "$num" in
 	Del_forwarding
 	;;
 	*)
-	echo "请输入正确数字 [0-5]"
+	echo "請輸入正確數字 [0-5]"
 	;;
 esac
